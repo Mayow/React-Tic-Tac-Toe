0a5cd6a173813ff7cad508688179ef05ea075faf
Mohamed <Mohamed@Mohameds-MacBook-Pro.local>
2019-03-21 23:15:20 -0400

    Test commit

--- src/components/Square.test.js
+++ src/components/Square.test.js
@@ -3,22 +3,22 @@ import Square from './Square'
 import { mount, shallow, render, Enzyme } from 'enzyme';
 
 
-// it('renders without crashing', () => {
-//   const count=0
-//   const turn= "X";
-//   const squareV= Array(9).fill(null);
-//   const setWon= " X";
+it('renders without crashing', () => {
+  const count=0
+  const turn= "X";
+  const squareV= Array(9).fill(null);
+  const setWon= " X";
 
-//   const newTurn = () => {
-//     ChangeTurns(turn, setTurn);
-//     setCount(count + 1);
-//     const sValue = squareV.slice();
-//     setHistory(history.concat([sValue]));
-//   }
+  const newTurn = () => {
+    ChangeTurns(turn, setTurn);
+    setCount(count + 1);
+    const sValue = squareV.slice();
+    setHistory(history.concat([sValue]));
+  }
 
-//   shallow( <Square setWon={setWon} num={0} newTurn={newTurn} 
-//             turn={turn} squareV={squareV} /> );
-// });
+  shallow( <Square setWon={setWon} num={0} newTurn={newTurn} 
+            turn={turn} squareV={squareV} /> );
+});
 
 describe('Square', () => {
 
