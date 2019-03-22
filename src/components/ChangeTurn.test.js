b0c4df197e63230e8e17de0c0085acafefb50c52
Mohamed <Mohamed@Mohameds-MacBook-Pro.local>
2019-03-21 23:14:32 -0400

    Test commit

--- src/components/ChangeTurn.test.js
+++ src/components/ChangeTurn.test.js
@@ -11,9 +11,9 @@ const setTurn = (turn) => {
   }
 }
 
-// test('change turn from X to O', () => {
-//     expect(ChangeTurns("X", setTurn)).toBe("O");
-//   });
+test('change turn from X to O', () => {
+    expect(ChangeTurns("X", setTurn)).toBe("O");
+  });
 
 // test('change turn from O to X', () => {
 //     expect(ChangeTurns("O", setTurn)).toBe("X");
